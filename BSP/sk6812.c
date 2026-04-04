22bf23dfb6b5654608487210b8b024143e16d7b2
da-zhexue <2152685808@qq.com>
2026-04-04 16:39:18 +0800

    神秘的RGB装甲板

--- BSP/sk6812.c
+++ BSP/sk6812.c
@@ -3,7 +3,7 @@
 #include "bsp_dwt.h"
 
 /*public variables*/
-uint8_t color = RED;
+uint8_t color = BLUE;
 uint8_t light_on = 1;
 
 /*private variables*/
@@ -68,9 +68,11 @@ void SK6812_Init(void)
     DWT_Delay_ms(1000);
 }
 
+void SK6812_LEDS_Flow(void);
 void SK6812_LEDS_Task(const uint8_t hit)
 {
-    if(light_on)SK6812_LEDS_Set();
+    //if(light_on) SK6812_LEDS_Set();
+    if(light_on) SK6812_LEDS_Flow();
     else SK6812_LEDS_Reset();
 
     // 被击打闪烁
@@ -84,3 +86,51 @@ void SK6812_LEDS_Task(const uint8_t hit)
     DWT_Delay_ms(12);
 }
 
+#define RGB_FLOW_COLOR_CHANGE_TIME  500
+#define RGB_FLOW_COLOR_LENGTH   6
+//blue-> green(dark)-> red -> blue(dark) -> green(dark) -> red(dark) -> blue
+
+uint32_t RGB_flow_color[RGB_FLOW_COLOR_LENGTH + 1] = {0xFF0000FF, 0x0000FF00, 0xFFFF0000, 0x000000FF, 0xFF00FF00, 0x00FF0000, 0xFF0000FF};
+void SK6812_LEDS_Flow(void)
+{
+    for(int i = 0; i < RGB_FLOW_COLOR_LENGTH; i++)
+    {
+        fp32 alpha = (fp32)((RGB_flow_color[i] & 0xFF000000) >> 24);
+        fp32 red = (fp32)((RGB_flow_color[i] & 0x00FF0000) >> 16);
+        fp32 green = (fp32)((RGB_flow_color[i] & 0x0000FF00) >> 8);
+        fp32 blue = (fp32)((RGB_flow_color[i] & 0x000000FF) >> 0);
+
+        fp32 delta_alpha = (fp32)((RGB_flow_color[i + 1] & 0xFF000000) >> 24) - (fp32)((RGB_flow_color[i] & 0xFF000000) >>
+            24);
+        fp32 delta_red = (fp32)((RGB_flow_color[i + 1] & 0x00FF0000) >> 16) - (fp32)((RGB_flow_color[i] & 0x00FF0000) >>
+            16);
+        fp32 delta_green = (fp32)((RGB_flow_color[i + 1] & 0x0000FF00) >> 8) - (fp32)((RGB_flow_color[i] & 0x0000FF00) >>
+            8);
+        fp32 delta_blue = (fp32)((RGB_flow_color[i + 1] & 0x000000FF) >> 0) - (fp32)((RGB_flow_color[i] & 0x000000FF) >> 0);
+
+        delta_alpha /= RGB_FLOW_COLOR_CHANGE_TIME;
+        delta_red /= RGB_FLOW_COLOR_CHANGE_TIME;
+        delta_green /= RGB_FLOW_COLOR_CHANGE_TIME;
+        delta_blue /= RGB_FLOW_COLOR_CHANGE_TIME;
+        for(int j = 0; j < RGB_FLOW_COLOR_CHANGE_TIME; j++)
+        {
+            alpha += delta_alpha;
+            red += delta_red;
+            green += delta_green;
+            blue += delta_blue;
+
+            for(uint8_t n = 0; n < SK6812_LEDS_NUM; n++)
+            {
+                // 如果需要带透明度亮度调节可以使用 (uint8_t)(red * (alpha/255.0))，此处直接使用计算出的RGB
+                Set_SINGLE_LED_RGB(n, (uint8_t)red, (uint8_t)green, (uint8_t)blue);
+            }
+            for(uint8_t n = 0; n < RESET_TIME; n++)
+            {
+                led_buf.RESET_Buffer[n] = 0;
+            }
+            Data_Transmit((uint32_t*)&led_buf, SK6812_BUFFER_SIZE);
+
+            DWT_Delay_ms(3);
+        }
+    }
+}
